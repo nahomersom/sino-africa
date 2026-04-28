a436285bce3ea89dbec5c9cdc5eef51f04728dde
nahom.ersom <9OcIvG8HOxje8g7g0vBxqLZLeVc5IsKLSCHWAjRiG57jn4Y3vtaXJQQJ99BHACAAAAAjt5wAAAASAZDO1drR>
2026-04-28 12:00:52 +0300

    fix/production

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "sino-cms.ablazelabs.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
